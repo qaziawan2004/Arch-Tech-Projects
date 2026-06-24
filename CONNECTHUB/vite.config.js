fbb03e3846ec6c9135d8d37598b246ef65c381d5
Mudassir Awan <qaziawan120@gmail.com>
2026-06-24 13:05:35 +0500

    ui

--- CONNECTHUB/vite.config.js
+++ CONNECTHUB/vite.config.js
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+      external: [], // Don't bundle server files
+    },
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
+});
