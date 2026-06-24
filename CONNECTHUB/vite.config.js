99bd89cf8298699b6619b6fde520451b474b8adf
Mudassir Awan <qaziawan120@gmail.com>
2026-06-24 14:12:29 +0500

    ui

--- CONNECTHUB/vite.config.js
+++ CONNECTHUB/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-      external: [], // Don't bundle server files
-    },
   },
   server: {
     proxy: {
