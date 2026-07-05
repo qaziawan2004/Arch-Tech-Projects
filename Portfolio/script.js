9d556a829423332bc284e8fe59282986494b308d
Mudassir Awan <qaziawan120@gmail.com>
2026-07-05 22:37:29 +0500

    add

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,45 +1,49 @@
 
-emailjs.init("zHH8BZEj4l9U6nHo5");
+if (typeof emailjs !== "undefined") {
 
-let form = document.getElementById("contactForm");
-let status = document.getElementById("status");
+    emailjs.init("zHH8BZEj4l9U6nHo5");
 
-form.addEventListener("submit", function (event) {
+    let form = document.getElementById("contactForm");
+    let status = document.getElementById("status");
 
-    event.preventDefault();
+    if (form) {
 
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let subject = document.getElementById("subject").value;
-    let message = document.getElementById("message").value;
+        form.addEventListener("submit", function (event) {
 
-    let data = {
-        from_name: name,
-        from_email: email,
-        subject: subject,
-        message: message
-    };
+            event.preventDefault();
 
-    emailjs.send("service_0zg3tib", "template_ls12nl5", data)
+            let name = document.getElementById("name").value;
+            let email = document.getElementById("email").value;
+            let subject = document.getElementById("subject").value;
+            let message = document.getElementById("message").value;
 
-        .then(function () {
+            let data = {
+                from_name: name,
+                from_email: email,
+                subject: subject,
+                message: message
+            };
 
-            status.innerHTML = "Message Sent Successfully!";
-            status.style.color = "lightgreen";
+            emailjs.send("service_0zg3tib", "template_ls12nl5", data)
+                .then(function () {
 
-            form.reset();
-            window.location.href = "index.html";
+                    status.innerHTML = "Message Sent Successfully!";
+                    status.style.color = "lightgreen";
 
-        })
+                    form.reset();
+                    window.location.href = "index.html";
 
-        .catch(function () {
+                })
+                .catch(function () {
 
-            status.innerHTML = "Failed To Send Message!";
-            status.style.color = "red";
+                    status.innerHTML = "Failed To Send Message!";
+                    status.style.color = "red";
 
+                });
         });
 
-});
+    }
+};
 document.addEventListener('DOMContentLoaded', function () {
 
     const hamburgerBtn = document.getElementById('hamburgerBtn');
